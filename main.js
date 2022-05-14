5bbfa23da914cc17d3a702aab132e516eb8cdfe4
Gustavo <completassessoria.rj@gmail.com>
2022-05-14 14:04:05 -0300

    aula loops

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 //primeira parte
 
+
+
 function calccmv() {
-    var precocusto = parseFloat(document.getElementById('custo').value);
-    var precovenda = parseFloat(document.getElementById('venda').value);
+    var precocusto = parseFloat(document.getElementById('custo').value)
+    var precovenda = parseFloat(document.getElementById('venda').value)
     var testevalid = document.getElementById("resultado").value = (precocusto / precovenda) - 1;
     var ret = document.getElementById("resultado").value = (precocusto / precovenda) * 100;
+
+    console.log(ret)
 
     if (testevalid > 0) {
         alert("ERRO: Valor de Custo não pode ser maior que Valor de Venda")
@@ -28,8 +32,6 @@ function aberto() {
 //segunda parte
 
 function calcmargem() {
-    var precocusto = parseFloat(document.getElementById('custo').value);
-    var precovenda = parseFloat(document.getElementById('venda').value);
     var alimp = parseFloat(document.getElementById('imposto').value);
     if (!alimp) { alert("Aliq de Imposto não pode estar vazio") };
     var alcom = parseFloat(document.getElementById('comissao').value);
@@ -43,8 +45,6 @@ function calcmargem() {
     var totalvariavel = parseFloat(document.getElementById('totalcustovar').value = (alimp + alcom + algar + alcmv + lucro));
 
     var margemcontribuicao = parseFloat(document.getElementById('margemcontribuicao').value = 100 - totalvariavel);
-
-    alert("estou na segunda parte")
 
     if (margemcontribuicao < 20) {alert('ATENÇÃO: Margem de Contribuição muito baixa')};
 }
